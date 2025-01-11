c7f3daf8ba38a1d835220152cd9400aa62e5624e
yclanlan <lanlan.yichun@gmail.com>
2025-01-11 10:23:08 -0500

    config update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/doomsday-glacier/",
+  base: "",
   plugins: [react(), eslint()],
 });
